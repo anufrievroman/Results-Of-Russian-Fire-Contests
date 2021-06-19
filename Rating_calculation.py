8f1452de13e24b8a60b1827b089a2f6ea6c1d2fe
Roman <anufriev.roman@protonmail.com>
2021-06-19 15:56:04 +0900

    Refactoring

--- Rating_calculation.py
+++ Rating_calculation.py
@@ -3,32 +3,32 @@ import pandas as pd
 # Reading the database
 data = pd.read_csv("Database.csv")
 
-first_places_a  = data['1st place'].dropna().to_list()
-second_places_a = data['2nd place'].dropna().to_list()
-third_places_a  = data['3rd place'].dropna().to_list()
-first_places_b  = data['1st place (additional)'].dropna().to_list()
-second_places_b = data['2nd place (additional)'].dropna().to_list()
-third_places_b  = data['3rd place (additional)'].dropna().to_list()
+first_a  = data['1st place'].dropna().to_list()
+second_a = data['2nd place'].dropna().to_list()
+third_a  = data['3rd place'].dropna().to_list()
+first_b  = data['1st place (additional)'].dropna().to_list()
+second_b = data['2nd place (additional)'].dropna().to_list()
+third_b  = data['3rd place (additional)'].dropna().to_list()
 
-# Let's put all participats in the same list and ret rid of repetitions
-all_participants    = first_places_a + first_places_b + second_places_a + second_places_b + third_places_a + third_places_b
-unique_participants = list(set(all_participants))
+# Let's collect only unique participants
+participants = list(set(first_a + first_b + second_a + second_b + third_a + third_b))
 
-# Here we count how many victories everyone got
+# Let's count how many victories everyone has
 rating = []
-for name in unique_participants:
-    num_of_first_places  = first_places_a.count(name) + first_places_b.count(name)
-    num_of_second_places = second_places_a.count(name) + second_places_b.count(name)
-    num_of_third_places  = third_places_a.count(name) + third_places_b.count(name)
+for name in participants:
+    num_of_first_places  = first_a.count(name) + first_b.count(name)
+    num_of_second_places = second_a.count(name) + second_b.count(name)
+    num_of_third_places  = third_a.count(name) + third_b.count(name)
     total_num_of_wins    = num_of_first_places + num_of_second_places + num_of_third_places
     score = 3*num_of_first_places + 2*num_of_second_places + 1*num_of_third_places
     rating.append([name, num_of_first_places, num_of_second_places, num_of_third_places, total_num_of_wins, score])
 
+# Let's sort first by total victories and then by score
 sorted_rating = sorted(rating, key=lambda column: float(column[5]), reverse=True)
 sorted_rating = sorted(sorted_rating, key=lambda column: float(column[4]), reverse=True)
 
 # Write the file
 with open("Rating_table.csv","w+") as f:
-    f.writelines(['Name,','1st place,','2nd place,','3rd place,','Total,','Score','\n'])
+    f.writelines('Name, 1st place, 2nd place, 3rd place, Total wins, Score\n')
     for line in sorted_rating:
         f.writelines(str(line)[1:-1]+'\n')
