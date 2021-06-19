8fdb189b835ee356965342a6107269d436a5423a
Roman <anufriev.roman@protonmail.com>
2021-06-19 15:41:23 +0900

    Refactoring the code

--- Rating_calculation.py
+++ Rating_calculation.py
@@ -1,42 +1,34 @@
 import pandas as pd
 
 # Reading the database
-df = pd.read_csv("Database.csv")
-first_places_a  = [str(df.iloc[i][2]) for i in range(df.shape[0])]
-second_places_a = [str(df.iloc[i][3]) for i in range(df.shape[0])]
-third_places_a  = [str(df.iloc[i][4]) for i in range(df.shape[0])]
-first_places_b  = [str(df.iloc[i][5]) for i in range(df.shape[0])]
-second_places_b = [str(df.iloc[i][6]) for i in range(df.shape[0])]
-third_places_b  = [str(df.iloc[i][7]) for i in range(df.shape[0])]
+data = pd.read_csv("Database.csv")
+
+first_places_a  = data['1st place'].dropna().to_list()
+second_places_a = data['2nd place'].dropna().to_list()
+third_places_a  = data['3rd place'].dropna().to_list()
+first_places_b  = data['1st place (additional)'].dropna().to_list()
+second_places_b = data['2nd place (additional)'].dropna().to_list()
+third_places_b  = data['3rd place (additional)'].dropna().to_list()
 
 # Let's put all participats in the same list and ret rid of repetitions
 all_participants    = first_places_a + first_places_b + second_places_a + second_places_b + third_places_a + third_places_b
 unique_participants = list(set(all_participants))
-unique_participants = [x for x in unique_participants if x != 'nan']
-
-rating = [0]*len(unique_participants)
 
 # Here we count how many victories everyone got
-for i in range(len(unique_participants)):
-    num_of_first_places  = first_places_a.count(unique_participants[i])+first_places_b.count(unique_participants[i])
-    num_of_second_places = second_places_a.count(unique_participants[i])+second_places_b.count(unique_participants[i])
-    num_of_third_places  = third_places_a.count(unique_participants[i])+third_places_b.count(unique_participants[i])
+rating = []
+for name in unique_participants:
+    num_of_first_places  = first_places_a.count(name) + first_places_b.count(name)
+    num_of_second_places = second_places_a.count(name) + second_places_b.count(name)
+    num_of_third_places  = third_places_a.count(name) + third_places_b.count(name)
     total_num_of_wins    = num_of_first_places + num_of_second_places + num_of_third_places
-
-    # So we give 3 points for the 1st place, 2 for second and 3 for third_places
     score = 3*num_of_first_places + 2*num_of_second_places + 1*num_of_third_places
-    rating[i] = [unique_participants[i], num_of_first_places, num_of_second_places, num_of_third_places, total_num_of_wins, score]
+    rating.append([name, num_of_first_places, num_of_second_places, num_of_third_places, total_num_of_wins, score])
 
-sorted_rating = sorted(rating, key=lambda column: float(column[4]), reverse=True)
-sorted_rating = sorted(sorted_rating, key=lambda column: float(column[5]), reverse=True)
+sorted_rating = sorted(rating, key=lambda column: float(column[5]), reverse=True)
+sorted_rating = sorted(sorted_rating, key=lambda column: float(column[4]), reverse=True)
 
 # Write the file
 with open("Rating_table.csv","w+") as f:
     f.writelines(['Name,','1st place,','2nd place,','3rd place,','Total,','Score,','\n'])
-    for i in range(len(unique_participants)):
-        f.writelines(['%s,' % sorted_rating[i][0],
-            '%s,' % sorted_rating[i][1],
-            '%s,' % sorted_rating[i][2],
-            '%s,' % sorted_rating[i][3],
-            '%s,' % sorted_rating[i][4],
-            '%s,' % sorted_rating[i][5],'\n'])
+    for line in sorted_rating:
+        f.writelines(str(line)[1:-1]+'\n')
